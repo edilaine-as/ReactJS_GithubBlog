4bc0f5e4f0c1930f0a2d87d67b8c15d1698ce73a
Edilaine Alves dos Santos <91744159+edilaine-as@users.noreply.github.com>
2024-02-02 15:23:52 -0300

    Working on Header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,13 @@
 import { styled } from "styled-components";
 
 export const HeaderContainer = styled.div`
+    height: 296px;
+
     img{
         width: 100%;
     }
+
+    position: relative;
+    z-index: -1;
+    margin-bottom: -5.5rem;
 `
